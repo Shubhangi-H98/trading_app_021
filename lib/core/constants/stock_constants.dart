3bcedecd976a3909c938f34cbb4c5d30c987a4cc
Shubhangi Hatwar <hatwarshubhangi77@gmail.com>
2026-08-22 13:43:00 +0530

    feat: add application splash screen

---
+++ lib/core/constants/stock_constants.dart
@@ -0,0 +1,14 @@
+class StockConstants {
+  static const List<String> stocks =[
+    'RELIANCE',
+    'TCS',
+    'INFY',
+    'HDFCBANK',
+    'ICICIBANK',
+    'SBIN',
+    'ITC',
+    'LT',
+    'BHARTIARTL',
+    'AXISBANK',
+  ];
+}
---
+++ lib/presentation/splash/pages/splash_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(
+      const Duration(seconds: 2),
+          () {
+        if (!mounted) return;
+
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              '021',
+              style: TextStyle(
+                fontSize: 42,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
+            const Text(
+              'Trading App',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 24),
+            const CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
